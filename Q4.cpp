a9f855dea9081eaba222fdc282c3b3437fda3d0a
Hardik kumar <140632249+hardikkumar4472@users.noreply.github.com>
2024-11-15 23:38:57 +0530

    Create Q4.cpp

---
+++ Q4.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int maxProfit(vector<int>& prices) {
+    int minPrice = INT_MAX;
+    int maxProfit = 0;
+    for (int price : prices) {
+        minPrice = min(minPrice, price);
+        maxProfit = max(maxProfit, price - minPrice);
+    }
+    return maxProfit;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> prices(n);
+    for (int i = 0; i < n; i++) {
+        cin >> prices[i];
+    }
+    cout << maxProfit(prices) << endl;
+    return 0;
+}
