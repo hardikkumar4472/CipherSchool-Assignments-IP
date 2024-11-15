853482c3761a1e4cf1133006edd5c7e5b9715462
Hardik kumar <140632249+hardikkumar4472@users.noreply.github.com>
2024-11-15 23:39:40 +0530

    Create Q5.cpp

---
+++ Q5.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int findKthLargest(vector<int>& nums, int k) {
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+    for (int num : nums) {
+        minHeap.push(num);
+        if (minHeap.size() > k) {
+            minHeap.pop();
+        }
+    }
+    return minHeap.top();
+}
+
+int main() {
+    int n, k;
+    cin >> n;
+    vector<int> nums(n);
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+    cin >> k;
+    cout << findKthLargest(nums, k) << endl;
+    return 0;
+}
